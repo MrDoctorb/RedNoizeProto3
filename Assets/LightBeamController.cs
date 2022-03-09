ad0175041b8051c5b7a13c2d006d71419eb67a20
MrDoctorb <zane@indybrowns.org>
2022-03-09 10:47:27 -0600

    Mirror bounce work

--- Assets/LightBeamController.cs
+++ Assets/LightBeamController.cs
@@ -18,16 +18,17 @@ public class LightBeamController : MonoBehaviour
         Ray ray = new Ray(transform.position, transform.forward);
         Physics.Raycast(ray, out rayHit);
 
-        if(rayHit.collider != null)
+        if (rayHit.collider != null)
         {
-            particleMain.startLifetime = particleMain.startSpeed; //* magnitude of start to end of the ray
+            particleMain.startLifetime = (rayHit.point - ray.origin).magnitude / particleMain.startSpeed.constant;
+
+            if (rayHit.collider.GetComponent(typeof(ITakeLight)))
+            {
+                ITakeLight obj = (ITakeLight)rayHit.collider.GetComponent(typeof(ITakeLight));
+                obj.LightHit(ray);
+            }
         }
 
-        if (rayHit.collider.GetComponent(typeof(ITakeLight)))
-        {
-            ITakeLight obj = (ITakeLight)rayHit.collider.GetComponent(typeof(ITakeLight));
-            obj.LightHit(ray);
-        }
 
     }
 }
