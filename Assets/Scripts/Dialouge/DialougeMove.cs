00f8d62cbd06128fd48ca2678fab68aa5f3c2762
MrDoctorb <zane@indybrowns.org>
2022-03-21 09:27:05 -0500

    NPC Move

--- Assets/Scripts/Dialouge/DialougeMove.cs
+++ Assets/Scripts/Dialouge/DialougeMove.cs
@@ -6,17 +6,26 @@ public class DialougeMove : StateMachineBehaviour
 {
 
     [SerializeField] Vector3 endPos;
+    [SerializeField] float speed;
+    Animator anime;
 
     //OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        anime = animator;
         Debug.Log(animator.GetComponent<NPCController>());
+        anime.GetComponent<NPCController>().StartCoroutine(MoveTo(endPos));
     }
 
 
     IEnumerator MoveTo(Vector3 pos)
     {
         yield return new WaitForEndOfFrame();
+        anime.transform.position = Vector3.MoveTowards(anime.transform.position, endPos, speed * Time.deltaTime);
+        if(Vector3.Distance(anime.transform.position, endPos) > .1f)
+        {
+            anime.GetComponent<NPCController>().StartCoroutine(MoveTo(pos));
+        }
 
     }
 }
