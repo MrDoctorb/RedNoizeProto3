e3f68e5a9f755d386ec6be0e0f71080cd7936df3
MrDoctorb <zane@indybrowns.org>
2022-03-27 12:21:01 -0500

    Scene Changes

---
+++ Assets/Scripts/PodiumRise.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodiumRise : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void TurnOn()
+    {
+        //Go up 2 units
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
