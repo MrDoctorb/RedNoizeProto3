2875c3ec3d6b8d14a12e8dc31e96d6505c81ef31
MrDoctorb <zane@indybrowns.org>
2022-04-17 21:19:56 -0500

    Doors Changed

---
+++ Assets/MovingPlatform.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] Vector3 endPos;
+    Vector3 startPos;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        startPos = transform.position;
+        StartCoroutine(MoveTo(endPos));
+    }
+
+    IEnumerator MoveTo(Vector3 point)
+    {
+        print((point - transform.position).normalized * speed);
+
+        while (Vector3.Distance(transform.position, point) > .1f)
+        {
+            rb.velocity = (point - transform.position).normalized * speed;
+            yield return new WaitForEndOfFrame();
+        }
+
+        if (point == endPos)
+        {
+            StartCoroutine(MoveTo(startPos));
+        }
+        else
+        {
+            StartCoroutine(MoveTo(endPos));
+        }
+    }
+}
