86edd1ba3fc029caf75d3bb36ea32e83fca222c3
MrDoctorb <zane@indybrowns.org>
2022-04-27 10:43:30 -0500

    Stop the button

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicManager : MonoBehaviour
+{
+
+    public static float bgVolume;
+    public static float sfxVolume;
+    [SerializeField] AudioMixerGroup bg;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
